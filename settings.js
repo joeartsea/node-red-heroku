a6e1ba05b9efc411146eeab24b824362bc1af2f1
David Fan <fand.kanade@gmail.com>
2020-05-27 12:15:56 -0400

    Updated settings.js

--- settings.js
+++ settings.js
@@ -36,6 +36,9 @@ var settings = module.exports = {
     // Move the admin UI
     httpAdminRoot: '/red',
 
+    // node-red-dashboard ui
+    ui: { path: "ui" },
+
     // You can protect the user interface with a userid and password by using the following property
     // the password must be an md5 hash  eg.. 5f4dcc3b5aa765d61d8327deb882cf99 ('password')
     //httpAdminAuth: {user:"user",pass:"5f4dcc3b5aa765d61d8327deb882cf99"},
